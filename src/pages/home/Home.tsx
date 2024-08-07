76c2b5bf5aee304a6bd66213faf6edc66b9cbcc4
Gonzalo Cortave <gonza.cortave@hotmail.com>
2024-08-06 21:41:40 -0300

    feat(loader): loader component added

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,15 +3,12 @@ import React from "react";
 import "./Home.css";
 import { NewsList } from "@/components/NewsList/NewsList";
 import { useNews } from "@/appProviders/NewsProvider/NewsProvider";
+import Loader from "@/components/Loader/Loader";
 
 const Home: React.FC = () => {
     const { news, isLoading } = useNews();
 
-    return (
-        <main className="Home">
-            {isLoading ? <div>Loading...</div> : <NewsList news={news} />}{" "}
-        </main>
-    );
+    return <main className="Home">{isLoading ? <Loader /> : <NewsList news={news} />} </main>;
 };
 
 export default Home;
